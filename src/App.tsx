e189a662bf8ad18e9c27b795bf44b22b5e679700
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2025-01-20 20:01:36 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DeckLayers from "./DeckLayers";
+
+function App() {
+  
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <DeckLayers />
+    </div>
+  );
+}
+
+export default App;
