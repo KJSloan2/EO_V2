2d3b16140b2485a0f79ce41b36eeb772c5fd751c
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2025-02-03 22:24:26 -0600

    Update ControlPanel.tsx

--- src/ControlPanel.tsx
+++ src/ControlPanel.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 type ControlPanelProps = {
-  showTerrainData: boolean;
-  onShowTerrainDataChange: (checked: boolean) => void;
   terrainOpacity: number;
   onTerrainOpacityChange: (value: number) => void;
   terrainPointRadius: number;
@@ -14,12 +12,22 @@ type ControlPanelProps = {
   isCollapsed: boolean;
   onCollapsedToggle: () => void;
   showParcels: boolean;
-  onShowParcelsChange: () => void;
+  onShowParcelsChange: (checked: boolean) => void;
+  showLandsat: boolean;
+  onShowLandsatChange: (checked: boolean) => void;
+  terrainColorMetric: string;
+  onTerrainColorMetricChange: (value: string) => void;
+  usgsFeatureData: string;
+  onUsgsFeatureChange: (value: string) => void;
+  showUsgsFeatures: string;
+  onShowUsgsFeaturesChange: (checked: boolean) => void;
+  usgsFeaturePointSize: number;
+  onUsgsFeaturePointSizeChange: (value: number) => void;
+  usgsFeaturePointOpacity: number;
+  onUsgsFeaturePointOpacityChange: (value: number) => void;
 };
 
 const ControlPanel: React.FC<ControlPanelProps> = ({
-  showTerrainData,
-  onShowTerrainDataChange,
   terrainOpacity,
   onTerrainOpacityChange,
   terrainPointRadius,
@@ -32,7 +40,35 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
   onCollapsedToggle,
   showParcels,
   onShowParcelsChange,
+  showLandsat,
+  onShowLandsatChange,
+  terrainColorMetric,
+  onTerrainColorMetricChange,
+  usgsFeatureData,
+  onUsgsFeatureChange,
+  showUsgsFeatures,
+  onShowUsgsFeaturesChange,
+  usgsFeaturePointSize,
+  onUsgsFeaturePointSizeChange,
+  usgsFeaturePointOpacity,
+  OnUsgsFeaturePointOpacityChange
+  
 }) => {
+
+  const usgsFeatureKeys = [
+    'ALL',
+    'Airport','Arch','Area','Arroyo','Bar','Basin','Bay',
+    'Beach','Bench','Bend','Bridge','Building','Canal',
+    'Cape','Cemetery','Census','Channel','Church','Civil',
+    'Cliff','Crater','Crossing','Dam','Falls','Flat',
+    'Forest','Gap','Gut','Harbor','Hospital','Island',
+    'Lake','Levee','Locale','Military','Mine','Oilfield',
+    'Park','Pillar','Plain','Populated Place','Post Office',
+    'Range','Rapids','Reserve','Reservoir','Ridge','School',
+    'Sea','Slope','Spring','Stream','Summit','Swamp',
+    'Tower','Trail','Tunnel','Valley','Well','Woods'];
+
+
   return (
     <div className={`summarypanel ${isCollapsed ? "collapsed" : ""}`}>
       <button className="toggle-button-statspanel" onClick={onCollapsedToggle}>
@@ -41,17 +77,16 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
 
       {!isCollapsed && (
         <div className="controlpanel">
-          <div className="controlpanel-title">Layers</div>
-          <br />
+          <div className="controlpanel-title"></div>
           <label>Terrain</label>
           <div className="control-dropdown">
             <label>
               <input
                 type="checkbox"
-                checked={showTerrainData}
-                onChange={(e) => onShowTerrainDataChange(e.target.checked)}
+                checked={showLandsat}
+                onChange={(e) => onShowLandsatChange(e.target.checked)}
               />
-              Site 3Dep Terrain
+              Landsat
             </label>
           </div>
           <div className="control-slider">
@@ -59,22 +94,12 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
               Terrain Opacity {terrainOpacity}
               <input
                 type="range"
-                min="0.01"
-                max="1"
-                step="0.01"
+                min="0.001"
+                max="0.1"
+                step="0.001"
                 value={terrainOpacity}
                 onChange={(e) => onTerrainOpacityChange(Number(e.target.value))}
               />
-            </label>
-          </div>
-          <div className="control-dropdown">
-            <label>
-              <input
-                type="checkbox"
-                checked={showFieldPhotoPoints}
-                onChange={(e) => onShowFieldPhotoPointsChange(e.target.checked)}
-              />
-              Field Photo Locations
             </label>
           </div>
           <div className="control-slider">
@@ -90,6 +115,40 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
               />
             </label>
           </div>
+          <div className="control-dropdown-container">
+            <div className="control-dropdown">
+              <label>
+                Terrain Color
+                <select
+                  value={terrainColorMetric}
+                  onChange={(e) => {
+                    const newValue = e.target.value;
+                    if (newValue !== terrainColorMetric) {
+                      onTerrainColorMetricChange(newValue);
+                    }
+                  }}
+                >
+                  {["altitude", "lstf_normalized", "ndvi"].map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
+          </div>
+          <br/>
+          <label>Field Photo Locations</label>
+          <div className="control-dropdown">
+            <label>
+              <input
+                type="checkbox"
+                checked={showFieldPhotoPoints}
+                onChange={(e) => onShowFieldPhotoPointsChange(e.target.checked)}
+              />
+              Field Photo Locations
+            </label>
+          </div>
           <br/>
           <label>Parcels</label>
           <div className="control-dropdown">
@@ -99,16 +158,74 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
                 checked={showParcels}
                 onChange={(e) => onShowParcelsChange(e.target.checked)}
               />
-              Field Photo Locations
+              Show Parcels
             </label>
           </div>
           <br/>
-          <div className="color-scale-labels">
+          <label>USGS Features</label>
+          <div className="control-dropdown">
             <label>
-              <input type="checkbox" checked={isRotating} onChange={onRotationToggle} />
-              Rotate View
+              <input
+                type="checkbox"
+                checked={showUsgsFeatures}
+                onChange={(e) => onShowUsgsFeaturesChange(e.target.checked)}
+              />
+              Show Features
             </label>
           </div>
+          <div className="control-slider">
+            <label>
+              Point Size {usgsFeaturePointSize}
+              <input
+                type="range"
+                min="100"
+                max="1000"
+                step="10"
+                value={usgsFeaturePointSize}
+                onChange={(e) => onUsgsFeaturePointSizeChange(Number(e.target.value))}
+              />
+            </label>
+          </div>
+          <div className="control-slider">
+            <label>
+              Opacity {usgsFeaturePointOpacity}
+              <input
+                type="range"
+                min="0.01"
+                max="1"
+                step="0.01"
+                value={usgsFeaturePointOpacity}
+                onChange={(e) => OnUsgsFeaturePointOpacityChange(Number(e.target.value))}
+              />
+            </label>
+          </div>
+          <br/>
+          <div className="control-dropdown-container">
+            <div className="control-dropdown">
+              <label>
+                <select
+                  value={usgsFeatureData}
+                  onChange={(e) => {
+                    const newValue = e.target.value;
+                    if (newValue !== usgsFeatureData) {
+                      onUsgsFeatureChange(newValue);
+                    }
+                  }}
+                >
+                  {usgsFeatureKeys.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
+          </div>
+          <br />
+          <label>
+            <input type="checkbox" checked={isRotating} onChange={onRotationToggle} />
+            Rotate View
+          </label>
         </div>
       )}
     </div>
